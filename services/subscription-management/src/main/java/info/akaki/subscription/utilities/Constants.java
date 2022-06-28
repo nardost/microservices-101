5d1b472639b89c79e2463500444d499329848226
nardost <nardos.tessema@gmail.com>
2022-06-28 17:36:52 -0500

    health check enabled in eureka client

--- services/subscription-management/src/main/java/info/akaki/subscription/utilities/Constants.java
+++ services/subscription-management/src/main/java/info/akaki/subscription/utilities/Constants.java
@@ -17,6 +17,4 @@ public final class Constants {
     public static final int MAX_LENGTH_PHONE_NUMBER = 15;
     public static final int MAX_LENGTH_CITY = 20;
     public static final int MAX_LENGTH_STREET_ADDRESS = 255;
-
-    public static final String SERVICE_DELIVERY_MICROSERVICE_HOST_URL = "http://127.0.0.1:8082";
 }

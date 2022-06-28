8fe9a6f7434ee11bf95df1d7db529463c09833d5
nardost <nardos.tessema@gmail.com>
2022-06-28 17:33:41 -0500

    RestTemplate has to be annotated @LoadBalanced

--- services/subscription-management/src/main/java/info/akaki/subscription/SubscriptionManagementApplication.java
+++ services/subscription-management/src/main/java/info/akaki/subscription/SubscriptionManagementApplication.java
@@ -2,6 +2,7 @@ package info.akaki.subscription;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +12,7 @@ public class SubscriptionManagementApplication {
         SpringApplication.run(SubscriptionManagementApplication.class, args);
     }
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
